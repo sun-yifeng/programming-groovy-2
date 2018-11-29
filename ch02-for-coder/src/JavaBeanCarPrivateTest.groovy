cf09489483afe8c1c26a2ba9b395120f61950c3b
sunyifeng <9339996@qq.com>
2018-11-30 00:20:37 +0800

    省略getter/setter

--- ch02-for-coder/src/JavaBeanCarPrivateTest.groovy
+++ ch02-for-coder/src/JavaBeanCarPrivateTest.groovy
@@ -7,7 +7,7 @@ car.drive(10)
 println("Miles:$car.miles")
 
 
-// 会报错:year不能被修改
+// 会报错：year不能被修改
 try {
     println("Can I set the year ？")
     car.year = 1900
@@ -15,6 +15,7 @@ try {
     println(e)
 }
 
+// 会报错：miles的setter方法中定义了异常
 try {
     println("Can I set the miles ? ")
     car.miles = 12
